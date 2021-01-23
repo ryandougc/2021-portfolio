0209141b80a1956cedf87201c11a77b25e4373ab
ryandougc <ryan.carr@emailkpu.ca>
2021-01-23 12:00:04 -0800

    Switch to Express server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express       = require('express')
+const app           = express()
+const server        = require('http').Server(app)
+const port          = process.env.PORT || 5000
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/resume', (req, res) => {
+    res.render('resume')
+})
+
+app.get('*', (req, res) => {
+    res.redirect('/')
+})
+
+server.listen(port, () => console.log(`App is running on port ${port}...`))
